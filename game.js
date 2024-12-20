7a8a7dbd7d87171c7cdf89b61c241eacd759e6b0
bernst4315 <bernst4315@gmail.com>
2024-12-20 10:55:14 -0700

    created ship object with properties
    made promt to enter ship name

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const shipName = prompt("Enter your ship name")
+console.log(shipName);
+
+let userShip = {
+    hull: 20,
+    firepower: 5,
+    accuracy: .7,
+};
+
+userShip = {name: shipName}
+
+console.log(userShip);
