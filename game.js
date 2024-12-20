38a5356ea6dec478d2754821974a852ab2e667fb
bernst4315 <bernst4315@gmail.com>
2024-12-20 11:42:32 -0700

    set up alienship properties

--- game.js
+++ game.js
@@ -1,12 +1,29 @@
-const shipName = prompt("Enter your ship name")
-console.log(shipName);
+// const shipName = prompt("Enter your ship name")
+// console.log(shipName);
+const alienAccuracy = getRandomNumber(.6, .8);
+const alienFirpower = getRandomNumber(2, 4);
+const alienHull = getRandomNumber(3, 6);
+
 
 let userShip = {
+    name: "Aster",
     hull: 20,
     firepower: 5,
     accuracy: .7,
 };
 
-userShip = {name: shipName}
+let alienShip = {
+    hull: alienHull,
+    firepower: alienFirpower,
+    accuracy: alienAccuracy
+};
 
-console.log(userShip);
+//userShip = {name: shipName}
+function getRandomNumber (min, max){
+    return (Math.random() * (max - min) + min);
+}
+    
+
+//console.log(userShip);
+console.log(alienShip);
+//console.log(accuracy);
